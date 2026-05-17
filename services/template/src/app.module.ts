b49be3dfb0938c6cc709b9d75ddadb3ee7a2a0b4
chinzillla <96362668+Chinzillla@users.noreply.github.com>
2026-05-17 17:54:04 -0400

    ERP-23 Removed the old hello world api route

--- services/template/src/app.module.ts
+++ services/template/src/app.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './health/health.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 
 @Module({
   imports: [HealthModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
