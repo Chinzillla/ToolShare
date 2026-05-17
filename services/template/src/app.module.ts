38e39faacdac41f7387b18bda85b3b25c4e47a2f
chinzillla <96362668+Chinzillla@users.noreply.github.com>
2026-05-17 17:59:13 -0400

    ERP-23 Installed and Implemented the nestjs-pino logger

--- services/template/src/app.module.ts
+++ services/template/src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from 'nestjs-pino';
 import { HealthModule } from './health/health.module';
+import { env } from './config/env'
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: env.LOG_LEVEL,
+      },
+    }),
+    HealthModule,
+  ],
 })
 export class AppModule {}
--- services/template/src/main.ts
+++ services/template/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 import { env } from './config/env';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  app.useLogger(app.get(Logger));
+
+  await app.listen(env.PORT);
 }
 bootstrap();
